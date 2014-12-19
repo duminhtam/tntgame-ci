d2648864de7d98b765265f60a9e1a9f9d8cb8106
duminhtam <tamdu@chotot.vn>
2014-12-19 21:11:03 +0700

    update removeweb

--- application/controllers/Stats.php
+++ application/controllers/Stats.php
@@ -39,21 +39,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Stats extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+
 	public function index()
 	{
 		$this->load->database();
@@ -62,6 +48,99 @@ class Stats extends CI_Controller {
 //		var_dump($data);
 
 		$this->load->view('stats', array('data'=>$data,'onlineCom'=>intval($onlineCom)));
+	}
+
+	public function getremoveWeb()
+	{
+		$this->load->database();
+		$warning_words = array();
+		$warning_words[] = "cap_3";
+		$warning_words[] = "cap-3";
+		$warning_words[] = "cap3";
+		$warning_words[] = "cat3";
+		$warning_words[] = "dam duc";
+		$warning_words[] = "dam dang";
+		$warning_words[] = "fuck";
+		$warning_words[] = "gay";
+		$warning_words[] = "giao hop";
+		$warning_words[] = "girl";
+		$warning_words[] = "goi tinh";
+		$warning_words[] = "goi-duc";
+		$warning_words[] = "goi-tinh";
+		$warning_words[] = "hiep dam";
+		$warning_words[] = "hung phan";
+		$warning_words[] = "jav";
+		$warning_words[] = "khieu-dam";
+		$warning_words[] = "khieudam";
+		$warning_words[] = "lam tinh";
+		$warning_words[] = "lauxanh";
+		$warning_words[] = "lauxanh.us";
+		$warning_words[] = "len dinh";
+		$warning_words[] = "make love";
+		$warning_words[] = "may mua";
+		$warning_words[] = "nude";
+		$warning_words[] = "nude";
+		$warning_words[] = "phim dau";
+		$warning_words[] = "phim-heo";
+		$warning_words[] = "porn";
+		$warning_words[] = "Porn.com";
+		$warning_words[] = "quan he";
+		$warning_words[] = "quanla";
+		$warning_words[] = "ra nuoc";
+		$warning_words[] = "rape";
+		$warning_words[] = "sex";
+		$warning_words[] = "sex.com";
+		$warning_words[] = "sung suong";
+		$warning_words[] = "thac loan";
+		$warning_words[] = "thiendia";
+		$warning_words[] = "tinh duc";
+		$warning_words[] = "tinh-duc";
+		$warning_words[] = "truyen dam";
+		$warning_words[] = "xnxx.com";
+		$warning_words[] = "xvideos.com";
+		$warning_words[] = "xx";
+		$warning_words[] = "xxx";
+		$warning_words[] = "youzeek";
+		$warning_words[] = "giao cau";
+		$warning_words[] = "fuk";
+		$warning_words[] = "fuck";
+		$warning_words[] = "fap";
+		$warning_words[] = "chan rau";
+		$warning_words[] = "cuong hiep";
+		$warning_words[] = "cuong buc";
+		$warning_words[] = "chi dau";
+		$warning_words[] = "anh re";
+		$warning_words[] = "em vo";
+		$warning_words[] = "chi vo";
+		$warning_words[] = "bo chong";
+		$warning_words[] = "nang dau";
+		$warning_words[] = "japanese";
+		$warning_words[] = "incest";
+		$warning_words[] = "destruction";
+		$warning_words[] = "husband";
+		$warning_words[] = "bitch";
+		$warning_words[] = "pussy";
+		$warning_words[] = "cutie";
+		$warning_words[] = "ass";
+		$warning_words[] = "cum";
+		$warning_words[] = "shot";
+		$warning_words[] = "hotel";
+		$warning_words[] = "amateur";
+		$warning_words[] = "pov";
+		$warning_words[] = "cock";
+		$warning_words[] = "doggy";
+		$warning_words[] = "dick";
+		$warning_words[] = "xvideo";
+
+		$webhistory = $this->db->select('URL')->from('webhistorytb');
+		foreach($warning_words AS $word){
+			$webhistory->or_like("URL", $word);
+		}
+		$data = $webhistory->limit(10)->get();
+
+		var_dump($data);
+
+//		$this->load->view('stats', array('data'=>$data,'onlineCom'=>intval($onlineCom)));
 	}
 
 }
